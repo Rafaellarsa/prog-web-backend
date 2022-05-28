fe8e3e10e7833dca9a2bb95eead0569e40f9ab17
vaneska-sousa <vaneskakaren15@gmail.com>
2022-05-28 19:27:50 -0300

    add product model and dao

---
+++ src/java/smdcommerce/produto/modelo/Produto.java
@@ -0,0 +1,107 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package smdcommerce.produto.modelo;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author SilvaVan
+ */
+public class Produto implements Serializable{
+    
+    private Integer id;
+    private String descricao;
+    private Double preco;
+    private String foto;
+    private Integer qntde;
+    private Integer id_categoria;
+
+    /**
+     * @return the id
+     */
+    public Integer getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the descricao
+     */
+    public String getDescricao() {
+        return descricao;
+    }
+
+    /**
+     * @param descricao the descricao to set
+     */
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    /**
+     * @return the preco
+     */
+    public Double getPreco() {
+        return preco;
+    }
+
+    /**
+     * @param preco the preco to set
+     */
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+
+    /**
+     * @return the foto
+     */
+    public String getFoto() {
+        return foto;
+    }
+
+    /**
+     * @param foto the foto to set
+     */
+    public void setFoto(String foto) {
+        this.foto = foto;
+    }
+
+    /**
+     * @return the qntde
+     */
+    public Integer getQntde() {
+        return qntde;
+    }
+
+    /**
+     * @param qntde the qntde to set
+     */
+    public void setQntde(Integer qntde) {
+        this.qntde = qntde;
+    }
+
+    /**
+     * @return the id_categoria
+     */
+    public Integer getId_categoria() {
+        return id_categoria;
+    }
+
+    /**
+     * @param id_categoria the id_categoria to set
+     */
+    public void setId_categoria(Integer id_categoria) {
+        this.id_categoria = id_categoria;
+    }
+
+    
+}
---
+++ src/java/smdcommerce/produto/modelo/ProdutoDAO.java
@@ -0,0 +1,117 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package smdcommerce.produto.modelo;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author SilvaVan
+ */
+public class ProdutoDAO {
+   
+    public boolean cadastrar (int id, String desc, double preco, String foto, int qtde, int id_categoria) throws Exception{
+        boolean status;
+        
+        Class.forName("org.postgresql.Driver");
+        Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/smdecommerce", "postgres", "ufc123");
+        PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO produto (id_produto, descricao_produto, preco_produto, foto_produto, quantidade_produto, id_categoria_produto) VALUES (?, ?, ?, ?, ?, ?)");
+        
+        preparedStatement.setInt(1, id);
+        preparedStatement.setString(2, desc);
+        preparedStatement.setDouble(3, preco);
+        preparedStatement.setString(4, foto);
+        preparedStatement.setInt(5, qtde);
+        preparedStatement.setInt(6, id_categoria);
+        
+        int resultado = preparedStatement.executeUpdate();
+        
+        preparedStatement.close();
+        connection.close();
+        
+        if (resultado != 1) {
+            status = false;
+        }else{
+            status = true;
+        }
+        
+        return status;
+    }
+    /**
+     *
+     * @param id
+     * @return
+     * @throws Exception
+     */
+    @SuppressWarnings("ConvertToTryWithResources")
+    public Produto consultar(int id) throws Exception {
+        Produto produto = null;
+        Class.forName("org.postgresql.Driver");
+        
+        Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/smdecommerce", "postgres", "ufc123");
+        PreparedStatement preparedStatement = connection.prepareStatement("SELECT id_produto, descricao_produto, preco_produto, foto_produto, quantidade_produto, id_categoria_produto FROM produto WHERE id_produto = ?");
+        preparedStatement.setInt(1, id);
+        
+        ResultSet resultSet = preparedStatement.executeQuery();
+        
+        while (resultSet.next()) {
+            produto = new Produto();
+            produto.setId(resultSet.getInt("id_produto"));
+            produto.setDescricao(resultSet.getString("descricao_produto"));
+            produto.setPreco(resultSet.getDouble("preco_produto"));
+            produto.setFoto(resultSet.getString("foto_produto"));
+            produto.setQntde(resultSet.getInt("quantidade_produto"));
+            produto.setId_categoria(resultSet.getInt("id_categoria_produto"));
+            
+            if (resultSet.wasNull()) {
+                produto.setFoto(null);
+            }
+        }
+        resultSet.close();
+        preparedStatement.close();
+        connection.close();
+        if (produto == null) {
+            throw new Exception("Produto não encontrado");
+        }
+        return produto;
+    }
+
+    @SuppressWarnings("ConvertToTryWithResources")
+    public List<Produto> listarProdutosEstoque() throws Exception {
+        List<Produto> produtos = new ArrayList<>();
+        Class.forName("org.postgresql.Driver");
+        
+        Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/smdecommerce", "postgres", "ufc123");
+        PreparedStatement preparedStatement = connection.prepareStatement("SELECT id_produto, descricao_produto, preco_produto, foto_produto, quantidade_produto, id_categoria_produto FROM produto WHERE quantidade_produto > 0");
+        ResultSet resultSet = preparedStatement.executeQuery();
+        
+        while (resultSet.next()) {
+            Produto produto = new Produto();
+
+            produto.setId(resultSet.getInt("id_produto"));
+            produto.setDescricao(resultSet.getString("descricao_produto"));
+            produto.setPreco(resultSet.getDouble("preco_produto"));
+            produto.setFoto(resultSet.getString("foto_produto"));
+            produto.setQntde(resultSet.getInt("quantidade_produto"));
+            produto.setId_categoria(resultSet.getInt("id_categoria_produto"));
+            
+            if (resultSet.wasNull()) {
+                produto.setFoto(null);
+            }
+            produtos.add(produto);
+        }
+        resultSet.close();
+        preparedStatement.close();
+        connection.close();
+        return produtos;
+    }
+
+}
