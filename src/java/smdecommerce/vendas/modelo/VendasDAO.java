2a926db2439341ecc4deb2028c8312fe50953ecd
vaneska-sousa <vaneskakaren15@gmail.com>
2022-07-11 18:00:14 -0300

    Add operações registro de vendas, remoção do registro de vendas e contagem de vendas por cliente

--- src/java/smdecommerce/vendas/modelo/VendasDAO.java
+++ src/java/smdecommerce/vendas/modelo/VendasDAO.java
@@ -1,16 +1,14 @@
 package smdecommerce.vendas.modelo;
 
-import smdecommerce.categoria.modelo.*;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
+import org.postgresql.shaded.com.ongres.scram.common.util.Preconditions;
 import smdecommerce.application.DatabaseConnection;
 
 public class VendasDAO {
-    
-    
+
     DatabaseConnection dbconnection = null;
     PreparedStatement preparedStatement = null;
 
@@ -18,63 +16,7 @@ public class VendasDAO {
         dbconnection = new DatabaseConnection();
     }
 
-    /*
-      Método utilizado para obter um usuário pelo seu identificador
-     
-      @param id
-      @return
-      @throws Exception
-     */
-    
-    /*
-    public Categoria obter(int id) throws Exception {
-        Categoria categoria = null;
-        
-        String SQLQuery = "SELECT id, descricao_categoria FROM categoria WHERE id_categoria = ?";
-        preparedStatement = dbconnection.getConnection().prepareStatement(SQLQuery);
-        
-        preparedStatement.setInt(1, id);
-        ResultSet resultSet = preparedStatement.executeQuery();
-        while (resultSet.next()) {
-            categoria = new Categoria();
-            categoria.setId(resultSet.getInt("id"));
-            categoria.setDescricaoCategoria(resultSet.getString("descricao_categoria"));
-        }
-        resultSet.close();
-        preparedStatement.close();
-        dbconnection.closeConnection();
-        if (categoria == null) {
-            throw new Exception("Categoria não encontrada");
-        }
-        return categoria;
-    }
-*/
-    /**
-     * Método utilizado para inserir um novo usuário
-     *
-     * @param nome
-     * @param endereco
-     * @param email
-     * @param login
-     * @param senha
-     * @param administrador
-     * @throws Exception
-     */
-    
-    /*
-    public void inserir(String descricaoCategoria) throws Exception {
-        String SQLQuery = "INSERT INTO categoria (id_categoria, descricao_categoria) VALUES (?, ?)";
-        preparedStatement = dbconnection.getConnection().prepareStatement(SQLQuery);
-        preparedStatement.setInt(1, new Random().nextInt(100000));
-        preparedStatement.setString(2, descricaoCategoria);
-        int resultado = preparedStatement.executeUpdate();
-        preparedStatement.close();
-        dbconnection.closeConnection();
-        if (resultado != 1) {
-            throw new Exception("Não foi possível inserir a categoria");
-        }
-    }
-    */
+    /* Lista compras feitas pelo cliente através do id do cliente */
     @SuppressWarnings("ConvertToTryWithResources")
     public List<Vendas> listarVendas(int clientID) throws Exception {
         List<Vendas> vendasList = new ArrayList<>();
@@ -97,23 +39,66 @@ public class VendasDAO {
         dbconnection.closeConnection();
         return vendasList;
     }
-    /*
-    public void remover(int id) throws Exception {
-        String SQLQuery = "DELETE FROM categoria WHERE id_categoria = ?";
+
+    //tentei fazer um count aqui
+    /*public Integer consultarVendasPorCliente(int clientID) throws Exception {
+
+    String SQLQuery = "SELECT COUNT(id_venda) FROM venda WHERE id_cliente_venda =" + String.valueOf(clientID);
+        preparedStatement = dbconnection.getConnection().prepareStatement(SQLQuery);
+        ResultSet resultSet = preparedStatement.executeQuery();
+
+        Preconditions.checkArgument(resultSet.next(), "Result set is empty");
+        return resultSet.getInt("size");
+    }*/
+    public Integer consultarVendasPorCliente(int clientID) throws Exception {
+        int contador = 0;
+        String SQLQuery = "SELECT COUNT(id_venda) FROM venda WHERE id_cliente_venda =" + String.valueOf(clientID);
+        preparedStatement = dbconnection.getConnection().prepareStatement(SQLQuery);
+        ResultSet resultSet = preparedStatement.executeQuery();
+
+        while (resultSet.next()) {
+            contador = resultSet.getInt(1);
+        }
+
+        resultSet.close();
+        preparedStatement.close();
+        dbconnection.closeConnection();
+
+        return contador;
+    }
+
+    public void cadastrarVenda(String data_e_hora, int id_cliente_venda, int id_produto_venda) throws Exception {
+
+        String SQLQuery = "INSERT INTO venda (data_e_hora, id_cliente_venda, id_produto_venda) VALUES (?, ?, ?)";
+        preparedStatement = dbconnection.getConnection().prepareStatement(SQLQuery);
+
+        preparedStatement.setString(1, data_e_hora);
+        preparedStatement.setInt(2, id_cliente_venda);
+        preparedStatement.setInt(3, id_produto_venda);
+
+        int resultado = preparedStatement.executeUpdate();
+
+        preparedStatement.close();
+        dbconnection.closeConnection();
+
+        if (resultado != 1) {
+            throw new Exception("Erro ao registrar a operação de venda");
+        }
+    }
+
+    public void deletarOperacao(int id) throws Exception {
+        String SQLQuery = "DELETE FROM venda WHERE id_venda = ?";
         preparedStatement = dbconnection.getConnection().prepareStatement(SQLQuery);
         preparedStatement.setInt(1, id);
-        preparedStatement.executeUpdate();
-        preparedStatement.close();
-        dbconnection.closeConnection();
-    }
-    
-    public void alterar(int id, String descricaoCategoria) throws Exception {
-        String SQLQuery = "UPDATE categoria SET descricao_categoria = ? WHERE id_categoria = ?";
-        preparedStatement = dbconnection.getConnection().prepareStatement(SQLQuery);
-        preparedStatement.setString(1, descricaoCategoria);
-        preparedStatement.setInt(2, id);
+
         int resultado = preparedStatement.executeUpdate();
+
         preparedStatement.close();
         dbconnection.closeConnection();
-    }*/
+
+        if (resultado != 1) {
+            throw new Exception("Erro ao deletar operação de venda");
+        }
+    }
+
 }
